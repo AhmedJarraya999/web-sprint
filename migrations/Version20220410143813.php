b153ce59f5f5fb7ee7f4a62b01dfbca569f62e5d
AhmedJarraya999 <96777472+AhmedJarraya999@users.noreply.github.com>
2022-04-10 16:22:25 +0100

    modification de l'affichage des stay

---
+++ migrations/Version20220410143813.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220410143813 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE booking (id INT AUTO_INCREMENT NOT NULL, stay_id INT DEFAULT NULL, user_id INT DEFAULT NULL, booking_date DATE NOT NULL, first_date DATE NOT NULL, end_date DATE NOT NULL, INDEX IDX_E00CEDDEFB3AF7D6 (stay_id), INDEX IDX_E00CEDDEA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE booking ADD CONSTRAINT FK_E00CEDDEFB3AF7D6 FOREIGN KEY (stay_id) REFERENCES stay (id)');
+        $this->addSql('ALTER TABLE booking ADD CONSTRAINT FK_E00CEDDEA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE booking');
+    }
+}
