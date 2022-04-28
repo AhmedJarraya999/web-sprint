a14e23d5fae9d235f945c39a3d5a31ad1986f984
AhmedJarraya999 <96777472+AhmedJarraya999@users.noreply.github.com>
2022-04-28 16:38:16 +0100

    texttype profile into

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\User;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Captcha\Bundle\CaptchaBundle\Form\Type\CaptchaType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\CallbackTransformer;
@@ -21,33 +22,38 @@ class UserType extends AbstractType
             ->add('lastname')
             ->add('email')
             ->add('phone')
-            ->add('introduction')
-            ->add('roles', ChoiceType::class, array(
-                'attr'  =>  array('class' => 'form-controll',
-                'style' => 'margin:5px 0;'),
-                'choices' => [
-                    'Host' => 'ROLE_HOST',
-                    'GUEST' => 'ROLE_GUEST',
-                    'ADMIN' => 'ROLE_ADMIN',
-                ]
-                //'multiple' => false,
-                //'required' => true,
+            ->add('introduction', TextareaType::class)
+            ->add(
+                'roles',
+                ChoiceType::class,
+                array(
+                    'attr'  =>  array(
+                        'class' => 'form-controll',
+                        'style' => 'margin:5px 0;'
+                    ),
+                    'choices' => [
+                        'Host' => 'ROLE_HOST',
+                        'GUEST' => 'ROLE_GUEST',
+                        'ADMIN' => 'ROLE_ADMIN',
+                    ]
+                    //'multiple' => false,
+                    //'required' => true,
                 )
             )
             ->add('captchaCode', CaptchaType::class, array(
                 'captchaConfig' => 'ExampleCaptcha'
             ));
 
-            $builder->get('roles')
-                ->addModelTransformer(new CallbackTransformer(
-                    function ($rolesArray) {
-                        // transform the array to a string
-                        return $rolesArray[0] ?? null;
-                    },
-                    function ($rolesString) {
-                        // transform the string back to an array
-                        return [$rolesString];
-                    }
+        $builder->get('roles')
+            ->addModelTransformer(new CallbackTransformer(
+                function ($rolesArray) {
+                    // transform the array to a string
+                    return $rolesArray[0] ?? null;
+                },
+                function ($rolesString) {
+                    // transform the string back to an array
+                    return [$rolesString];
+                }
             ));
     }
 
