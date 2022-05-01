19cfe208d3a41a27bcdd8f582255fb81279e998e
AhmedJarraya999 <96777472+AhmedJarraya999@users.noreply.github.com>
2022-05-01 08:20:31 +0100

    better registration form ui

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -11,6 +11,8 @@ use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -23,9 +25,9 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('firstname')
-            ->add('lastname')->add('email')->add('phone')
-            ->add('username')
+            ->add('firstname', TextType::class)
+            ->add('lastname', TextType::class)->add('email', TextType::class)->add('phone', TextType::class)
+            ->add('username', TextType::class)
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
@@ -43,32 +45,37 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
-            ->add('roles', ChoiceType::class, array(
-                'attr'  =>  array('class' => 'form-controll',
-                'style' => 'margin:5px 0;'),
-                'choices' => [
-                    'Host' => 'ROLE_HOST',
-                    'GUEST' => 'ROLE_GUEST',
-                    //'ADMIN' => 'ROLE_ADMIN',
-                ]
-                //'multiple' => false,
-                //'required' => true,
+            ->add(
+                'roles',
+                ChoiceType::class,
+                array(
+                    'attr'  =>  array(
+                        'class' => 'form-controll',
+                        'style' => 'margin:5px 0;'
+                    ),
+                    'choices' => [
+                        'Host' => 'ROLE_HOST',
+                        'GUEST' => 'ROLE_GUEST',
+                        //'ADMIN' => 'ROLE_ADMIN',
+                    ]
+                    //'multiple' => false,
+                    //'required' => true,
                 )
             )
             ->add('captchaCode', CaptchaType::class, array(
                 'captchaConfig' => 'ExampleCaptcha'
             ));
 
-            $builder->get('roles')
-                ->addModelTransformer(new CallbackTransformer(
-                    function ($rolesArray) {
-                        // transform the array to a string
-                        return $rolesArray[0] ?? null;
-                    },
-                    function ($rolesString) {
-                        // transform the string back to an array
-                        return [$rolesString];
-                    }
+        $builder->get('roles')
+            ->addModelTransformer(new CallbackTransformer(
+                function ($rolesArray) {
+                    // transform the array to a string
+                    return $rolesArray[0] ?? null;
+                },
+                function ($rolesString) {
+                    // transform the string back to an array
+                    return [$rolesString];
+                }
             ));
     }
 
